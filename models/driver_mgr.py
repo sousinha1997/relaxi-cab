909f5448215bd51db2126ea59a86e3ff721a66fb
soumyaln <164865715+soumyaln@users.noreply.github.com>
2024-07-17 19:16:17 +0530

    Update driver_mgr.py
    make private

--- models/driver_mgr.py
+++ models/driver_mgr.py
@@ -8,7 +8,7 @@ class DriverManager:
     DriverManagerInst = None
 
     def __init__(self):
-        self.driverStore = {}
+        self.__driverStore = {}
 
     @staticmethod
     def get_instance():
@@ -17,12 +17,12 @@ class DriverManager:
         return DriverManager.DriverManagerInst
 
     def add_driver(self, driver_name, driver_object):
-        self.driverStore[driver_name] = driver_object
+        self.__driverStore[driver_name] = driver_object
 
     def get_driver(self, phone_number):
-        if not self.driverStore.get(phone_number):
+        if not self.__driverStore.get(phone_number):
             return None
-        return self.driverStore[phone_number]
+        return self.__driverStore[phone_number]
 
     def get_driver_map(self):
-        return self.driverStore
+        return self.__driverStore
